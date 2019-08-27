f2572ee5b16cdb37be9e44b8ffeea1229b227dc6
David Cornish <52015556+dcornish84@users.noreply.github.com>
2019-08-27 14:01:21 -0500

    Cookout Practice

---
+++ cookout.js
@@ -0,0 +1,42 @@
+const hamburger = {
+	name: 'Hamburger',
+	type: 'beef',
+	cooked: false,
+}
+const zucchini = {
+	name: 'Zucchini',
+	type: 'vegetable',
+	cooked: false,
+}
+const chickenBreast = {
+	name: 'Chicken Breast',
+	type: 'chicken',
+	cooked: false,
+}
+const corn = {
+	name: 'Corn',
+	type: 'vegetable',
+	cooked: false,
+}
+const steak = {
+	name: 'Steak',
+	type: 'beef',
+	cooked: false,
+}
+
+// An array that is grouping the objects together.
+const foods = [hamburger, zucchini, chickenBreast, corn, steak];
+
+// An empty array that will store the objects after the `grill()` function cooks the food.
+const cookedFood = [];
+
+function grill () {
+    for(let i = 0; i < foods.length; (i++)){
+       foods[i].cooked = true;
+        cookedFood.push(foods[i].name);
+
+        } 
+    }
+    
+    grill ();
+   console.log(`these are cooked foods) ${cookedFood}`)
